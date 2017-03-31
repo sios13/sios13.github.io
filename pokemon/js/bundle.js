587e5515f933f63b8b1936582abcbbf7aa14726a
sios13 <simon2@live.se>
2017-03-31 16:53:28 +0200

    pokemon!

---
+++ pokemon/js/bundle.js
@@ -0,0 +1,475 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Entity(x, y, mapX, mapY, width, height, speed, direction) {
+    this.x = x;
+    this.y = y;
+
+    this.mapX = mapX;
+    this.mapY = mapY;
+
+    this.width = width;
+    this.height = height;
+
+    this.speed = speed;
+
+    this.speedX = null;
+    this.speedY = null;
+
+    this.col = null;
+    this.row = null;
+
+    this.direction = direction;
+
+    this.moveAnimationCounter = 0;
+
+    let sprites = new Image();
+    sprites.src = "img/characters.png";
+
+    this.sprite = {
+        img: sprites,   // Specifies the image, canvas, or video element to use
+        sx: 4*16,       // Optional. The x coordinate where to start clipping
+        sy: 0,          // Optional. The y coordinate where to start clipping
+        swidth: 16,     // Optional. The width of the clipped image
+        sheight: 16,    // Optional. The height of the clipped image
+    }
+}
+
+Entity.prototype._detectCollision = function(game) {
+    let x = this.x;
+    let y = this.y;
+
+    let collisionPoints = [
+        [x, y],                           // Top left
+        [x+this.width, y],                // Top right
+        [x, y+this.height],               // Bottom left
+        [x+this.width, y+this.height],    // Bottom right
+        [x+this.width/2, y],              // Top
+        [x+this.width, y+this.height/2],  // Right
+        [x+this.width/2, y+this.height],  // Bottom
+        [x, y+this.height/2]              // Left
+    ];
+
+    // Iterate the collision points
+    for (let i = 0; i < collisionPoints.length; i++) {
+        let pointX = collisionPoints[i][0];
+        let pointY = collisionPoints[i][1];
+
+        let oldColumn = Math.floor(pointX / game.map.gridSize);
+        let oldRow = Math.floor(pointY / game.map.gridSize);
+
+        let newColumn = Math.floor((pointX+this.speedX) / game.map.gridSize);
+        let newRow = Math.floor((pointY+this.speedY) / game.map.gridSize);
+
+        // If collision point is trying to enter a disallowed grid
+        if (game.map.collisionMap[newRow][newColumn] === 1) {
+            // If trying to enter new column and row at the same time
+            if (newColumn !== oldColumn && newRow !== oldRow) {
+                // Trust that another collision point will find the collision
+                continue;
+            }
+
+            // If trying to enter a new column
+            if (newColumn !== oldColumn) {
+                this.speedX = 0;
+            }
+
+            // If trying to enter a new row
+            if (newRow !== oldRow) {
+                this.speedY = 0;
+            }
+        }
+    }
+}
+
+Entity.prototype._setDirection = function() {
+    let radians = Math.atan2(this.speedY, this.speedX);
+
+    let degrees = radians * (180 / Math.PI);
+
+    if (degrees < -135 || degrees > 135) {
+        this.direction = "left";
+    } else if (degrees < -45) {
+        this.direction = "up";
+    } else if (degrees < 45) {
+        this.direction = "right";
+    } else if (degrees < 135) {
+        this.direction = "down";
+    }
+}
+
+Entity.prototype.update = function(game) {
+    if (game.listeners.isMousedown) {
+        let deltaX = game.mousePositionX - this.mapX - this.width/2;
+        let deltaY = game.mousePositionY - this.mapY - this.height/2;
+
+        let distance = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+
+        if (distance < 5) {
+            return;
+        }
+
+        this.speedX = deltaX/distance*this.speed;
+        this.speedY = deltaY/distance*this.speed;
+
+        this._setDirection();
+
+        this._detectCollision(game);
+
+        this.col = Math.floor((this.x+this.width/2) / game.map.gridSize);
+        this.row = Math.floor((this.y+this.height/2) / game.map.gridSize);
+
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Animation
+        if (game.frame % 5 === 0) {
+            this.moveAnimationCounter += 1;
+        }
+
+        this.sprite.sx = (3 + this.moveAnimationCounter % 3) * 16 + 3;
+
+        if (this.direction === "up") {
+            this.sprite.sy = 3*16;
+        } else if (this.direction === "right") {
+            this.sprite.sy = 2*16;
+        } else if (this.direction === "down") {
+            this.sprite.sy = 0*16;
+        } else if (this.direction === "left") {
+            this.sprite.sy = 1*16;
+        }
+
+        return;
+    }
+
+    this.sprite.sx = 4 * 16 + 3;
+}
+
+Entity.prototype.render = function(context) {
+    context.drawImage(this.sprite.img, this.sprite.sx, this.sprite.sy, this.sprite.swidth - 6, this.sprite.sheight, this.mapX, this.mapY-20, this.width, this.height+20);
+
+    // context.beginPath();
+    // context.rect(this.mapX, this.mapY, this.width, this.height);
+    // context.stroke();
+}
+
+module.exports = Entity;
+
+},{}],2:[function(require,module,exports){
+const Entity = require("./Entity.js");
+const MapInitializer = require("./MapInitializer.js");
+
+function Game() {
+    this.frame = 0;
+    this.framerate = 30;
+
+    this.canvas = document.querySelector("canvas");
+    this.context = this.canvas.getContext("2d");
+
+    this.map = MapInitializer.getMap("startMap");
+
+    this.coolguy = new Entity(14*32, 35*32, this.canvas.width/2, this.canvas.height/2, 30, 30, 5);
+}
+
+Game.prototype.startGame = function() {
+    require("./listeners.js").addListeners(this);
+
+    // Start game!
+    setInterval(frame.bind(this), 1000/this.framerate);
+
+    function frame() {
+        this.frame += 1;
+
+        update();
+        render();
+    }
+
+    let update = () => {
+        // Update position and animation
+        this.coolguy.update(this);
+
+        // Check for events (depending on block)
+        this._checkEvents();
+
+        // Update map position
+        this.map.x = this.coolguy.mapX - this.coolguy.x;
+        this.map.y = this.coolguy.mapY - this.coolguy.y;
+    }
+
+    let render = () => {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.map.renderLayer1(this.context);
+
+        this.coolguy.render(this.context);
+
+        this.map.renderLayer2(this.context);
+
+        this.map.render(this.context);
+    }
+};
+
+Game.prototype._checkEvents = function() {
+    let col = this.coolguy.col;
+    let row = this.coolguy.row;
+
+    if (col === null || row === null) {
+        return;
+    }
+
+    let event = this.map.getEvent(col, row);
+
+    if (typeof event === "object" && event.id === 2) {
+        this.map.destroy();
+
+        // id:2 -> change map! teleport!
+        this.map = MapInitializer.getMap(event.data.mapName);
+
+        this.coolguy.x = event.data.spawnX;
+        this.coolguy.y = event.data.spawnY;
+
+        return;
+    }
+}
+
+module.exports = Game;
+
+},{"./Entity.js":1,"./MapInitializer.js":4,"./listeners.js":6}],3:[function(require,module,exports){
+function Map(x, y, collisionMap, gridSize, layer1Src, layer2Src, audioSrc) {
+    this.x = x;
+    this.y = y;
+
+    this.collisionMap = collisionMap;
+
+    this.gridSize = gridSize;
+
+    this.layer1Image = new Image();
+    this.layer1Image.src = layer1Src;
+
+    this.layer2Image = new Image();
+    this.layer2Image.src = layer2Src;
+
+    this.audio = new Audio(audioSrc);
+    this.audio.loop = true;
+    this.audio.play();
+}
+
+Map.prototype.attachEvent = function(col, row, event) {
+    this.collisionMap[row][col] = event;
+}
+
+Map.prototype.getEvent = function(col, row) {
+    return this.collisionMap[row][col];
+}
+
+Map.prototype.render = function(context) {
+    for (let y = 0; y < this.collisionMap.length; y++) {
+        for (let x = 0; x < this.collisionMap[y].length; x++) {
+            if (this.collisionMap[y][x] === 1) {
+                // context.beginPath();
+                // context.rect(this.x + x*this.gridSize, this.y + y*this.gridSize, this.gridSize, this.gridSize);
+                // context.stroke();
+            }
+        }
+    }
+}
+
+Map.prototype.renderLayer1 = function(context) {
+    context.drawImage(this.layer1Image, this.x, this.y);
+}
+
+Map.prototype.renderLayer2 = function(context) {
+    context.drawImage(this.layer2Image, this.x, this.y);
+}
+
+Map.prototype.destroy = function() {
+    this.audio.pause();
+}
+
+module.exports = Map;
+
+},{}],4:[function(require,module,exports){
+const Map = require("./Map.js");
+
+function getMap(mapName) {
+    if (mapName === "startMap") {
+        return startMap();
+    }
+
+    if (mapName === "coolMap") {
+        return coolMap();
+    }
+}
+
+function startMap() {
+    let x = 0;
+    let y = 0;
+
+    let collisionMap = [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,0,0,0,0,0,0,0,1,1,0,0,0,1,1,1,1,0,0,0,0,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,0,0,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1],
+        [1,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1],
+        [1,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,1,2,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ];
+
+    let gridSize = 32;
+
+    let layer1Src = "img/map1layer1.png";
+    let layer2Src = "img/map1layer2.png";
+
+    let audioSrc = "audio/hehe.mp3";
+
+    let map = new Map(x, y, collisionMap, gridSize, layer1Src, layer2Src, audioSrc);
+
+    for (let y = 0; y < collisionMap.length; y++) {
+        for (let x = 0; x < collisionMap[y].length; x++) {
+            if (collisionMap[y][x] === 2) {
+                map.attachEvent(x, y, {
+                    id: 2,
+                    data: {
+                        mapName: "coolMap",
+                        spawnX: 10*32,
+                        spawnY: 8*32
+                    }
+                });
+            }
+        }
+    }
+
+    return map;
+}
+
+function coolMap() {
+    let x = 0;
+    let y = 0;
+
+    let collisionMap = [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,1],
+        [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ];
+
+    let gridSize = 32;
+
+    let layer1Src = "img/house1layer1.png";
+    let layer2Src = "img/house1layer2.png";
+
+    let audioSrc = "audio/hehe.mp3";
+
+    let map = new Map(x, y, collisionMap, gridSize, layer1Src, layer2Src, audioSrc);
+
+    for (let y = 0; y < collisionMap.length; y++) {
+        for (let x = 0; x < collisionMap[y].length; x++) {
+            if (collisionMap[y][x] === 2) {
+                map.attachEvent(x, y, {
+                    id: 2,
+                    data: {
+                        mapName: "startMap",
+                        spawnX: 6*32,
+                        spawnY: 39*32
+                    }
+                });
+            }
+        }
+    }
+
+    return map;
+}
+
+module.exports = {
+    getMap: getMap
+};
+
+},{"./Map.js":3}],5:[function(require,module,exports){
+let Game = require("./Game.js");
+
+// node_modules/.bin/browserify source/js/app.js > debug/js/bundle.js
+
+window.addEventListener("load", function() {
+    let game = new Game();
+
+    game.startGame();
+});
+
+},{"./Game.js":2}],6:[function(require,module,exports){
+function addListeners(game) {
+    game.listeners = {};
+
+    game.canvas.addEventListener("mousedown", function(e) {
+        game.listeners.isMousedown = true;
+
+        game.listeners.mousePositionX = window.event.clientX;
+        game.listeners.mousePositionY = window.event.clientY;
+    });
+
+    game.canvas.addEventListener("mousemove", function(e) {
+        game.listeners.isMousemove = true;
+
+        game.mousePositionX = window.event.clientX;
+        game.mousePositionY = window.event.clientY;
+    });
+
+    game.canvas.addEventListener("mouseup", function(e) {
+        game.listeners.isMousedown = false;
+        game.listeners.isMousemove = false;
+    });
+
+    game.canvas.addEventListener("keydown", function(e) {
+        console.log("keydown");
+    });
+}
+
+module.exports = {
+    addListeners: addListeners
+}
+
+},{}]},{},[5]);
